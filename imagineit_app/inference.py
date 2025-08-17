4e787d05c631d1b3d5b98ee0e30a7d78c21817e5
rkoba0020 <rkoba0020@gmail.com>
2025-08-16 22:24:19 -0400

    Add LoRA support

--- imagineit_app/inference.py
+++ imagineit_app/inference.py
@@ -2,23 +2,38 @@ import os
 import torch
 from diffusers import StableDiffusionXLPipeline # Use the correct XL pipeline
 from io import BytesIO
+import gc
 
-base_model_id = "cagliostrolab/animagine-xl-4.0"
-device = "cuda" if torch.cuda.is_available() else "cpu"
+pipe = None
 
-print("Loading the SDXL base pipeline structure...")
-pipe = StableDiffusionXLPipeline.from_pretrained( # Use the XL class
-    base_model_id,
-    torch_dtype=torch.float16 if device == "cuda" else torch.float32,
-)
-if device == "cuda":
-    pipe.enable_model_cpu_offload()
+def load_model(loras: str=None):
+    global pipe
+    if pipe is not None:
+        pipe = None
+        gc.collect()    
+        with torch.no_grad():
+            torch.cuda.empty_cache()
+    base_model_id = "cagliostrolab/animagine-xl-4.0"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    print("Loading the SDXL base pipeline structure...")
+    pipe = StableDiffusionXLPipeline.from_pretrained( # Use the XL class
+        base_model_id,
+        torch_dtype=torch.float16 if device == "cuda" else torch.float32,
+    )
+    if loras is not None:
+        pipe.load_lora_weights(loras)
+    if device == "cuda":
+        pipe.to("cuda")
 
 def img_inference(prompt: str, steps: int=28, guidance_scale: float=5.0, negative_prompt: str = "", width: int = 1024, height: int = 1024, seed: int=42, batch_size: int=1):
+    global pipe
+    if pipe is None:
+        load_model(None)
     prompts = [prompt] * batch_size
     negative_prompts = [negative_prompt] * batch_size
     numerical_seeds = [(seed)] if batch_size == 1 else [int.from_bytes(os.urandom(8), signed=False) for _ in range(batch_size)]
-    seeds = [torch.Generator(device=device).manual_seed(nseed) for nseed in numerical_seeds]
+    seeds = [torch.Generator(device="cpu").manual_seed(nseed) for nseed in numerical_seeds]
     images = pipe(
         prompt=prompts, 
         negative_prompt=negative_prompts,
--- imagineit_app/static/constants.ts
+++ imagineit_app/static/constants.ts
@@ -63,6 +63,7 @@ export const COOKIE_BATCH_SIZE = 'ai_studio_batch_size';
 export const COOKIE_INFERENCE_COUNT = 'ai_studio_inference_count';
 export const COOKIE_ACTIVE_TAB = 'ai_studio_active_tab';
 export const COOKIE_ALWAYS_RANDOM_SEED = 'ai_studio_always_random_seed';
+export const COOKIE_LORA_MODEL = 'ai_studio_lora_model';
 
 // Cookie Keys for Backend Settings
 export const COOKIE_BACKEND_MODE = 'ai_studio_backend_mode';
